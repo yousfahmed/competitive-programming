eb41fdeb75811b71ef3ce354206a994886495880
Abdeelaleem Ahmed <yousfahmed2@gmail.com>
2022-06-27 23:15:10 +0200

    Create A. N integers.cpp

---
+++ Gyms/JU Flash Contest/A. N integers.cpp
@@ -0,0 +1,24 @@
+/*
+  Link : https://codeforces.com/gym/102035/problem/A
+  Author : Abdelaleleem 
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define all(v)  (v).begin(),(v).end()
+
+inline void solve() {
+
+
+}
+
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr), cout.tie(nullptr);
+    int n;
+    cin >> n;
+    cout << n * 4;
+}
+ 
