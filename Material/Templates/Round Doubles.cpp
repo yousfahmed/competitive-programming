c37d2fb279246f48c24b9804f29d313e69f33650
Abdeelaleem Ahmed <yousfahmed2@gmail.com>
2022-07-28 07:45:40 +0200

    Update Round Doubles.cpp

--- Material/Templates/Round Doubles.cpp
+++ Material/Templates/Round Doubles.cpp
@@ -2,8 +2,8 @@ double round(double a, int afterPoint) {
     const double eps = 1e-9;
     int TT = 1;
     while (afterPoint--) TT *= 10;
-    if (fabs((a * (1.0 * TT)) - (int) (a * TT) - 0.5) > eps && (int) (a * TT * 10) % 10 >= 5)
-        return ceil(a * (1.0 * TT)) / (1.0 * TT);
-    else
-        return floor(a * (1.0 * TT)) / (1.0 * TT);
+    if (fabs((a * (1.0 * TT)) - (int) (a * TT) - 0.5) > eps && (int) (a * TT * 10) % 10 >= 5){
+        return ceil(a * (1.0 * TT)) / (1.0 * TT); 
+    }
+    return floor(a * (1.0 * TT)) / (1.0 * TT);
 }
