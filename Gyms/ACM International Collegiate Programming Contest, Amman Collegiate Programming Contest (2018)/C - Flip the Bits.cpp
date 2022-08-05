dac88d89739cd585e75391f5aa28b3a31d6ae040
Abdeelaleem Ahmed <yousfahmed2@gmail.com>
2022-08-06 00:15:40 +0200

    Create C - Flip the Bits.cpp

---
+++ Gyms/ACM International Collegiate Programming Contest, Amman Collegiate Programming Contest (2018)/C - Flip the Bits.cpp
@@ -0,0 +1,31 @@
+/*
+   Author : Abdelaleem Ahmed
+   Codeforces Handle  : [Abdelaleem](https://codeforces.com/profile/Abdelaleem)
+   Link To problem : https://codeforces.com/gym/101810/problem/C
+*/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define int long long
+
+signed main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr), cout.tie(nullptr);
+#ifndef ONLINE_JUDGE
+    freopen("ans.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        int i = 0;
+        for (; !(n & (1 << i)); ++i);
+        cout << (i + 1) << '\n';
+    }
+
+}
+
