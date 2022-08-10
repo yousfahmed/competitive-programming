6476d34a147ba9e32a9fd37713469ccdea0701f4
Abdeelaleem Ahmed <yousfahmed2@gmail.com>
2022-08-10 17:02:03 +0200

    Update Hash Struct.cpp

--- Material/Templates/Hash Struct.cpp
+++ Material/Templates/Hash Struct.cpp
@@ -1,30 +1,33 @@
-#define ll long long
 struct hsh {
 private:
-    ll h1, h2, p1 = 1e9 + 7, p2 = 1e9 + 9, x;
-    ll *pw1, *pw2;
+    long long p1 = 1e9 + 7, p2 = 1e9 + 9;
     int len;
-    deque<int> q;
 
-    ll power(ll X, ll y, ll p) {
-        if (y == 0) return 1;
-        ll temp = power(X, y / 2, p) % p;
-        if (y % 2 == 0)
-            return (temp * temp) % p;
-        return (((X * temp) % p) * temp) % p;
+    long long h1, h2, x;
+    long long inv1, inv2;
+    long long *pw1, *pw2;
+    deque<char> q;
+
+    int power(int n, int p, int md) {
+        if (!p)return 1;
+        long long t = power(n, p / 2, md);
+        t = t * t % md;
+        if (p & 1) t = t * n % md;
+        return t;
     }
 
 public:
-    hsh(int sz, int x = 128) {
+    hsh(int sz, int x = 128) : x(x) {
         h1 = h2 = 0;
         len = 0;
-        this->x = x;
-        pw1 = new ll[sz + 1];
-        pw2 = new ll[sz + 1];
+        inv1 = power(x, p1 - 2, p1);
+        inv2 = power(x, p2 - 2, p2);
+        pw1 = new int[sz + 1];
+        pw2 = new int[sz + 1];
         pw1[0] = pw2[0] = 1;
         for (int i = 1; i <= sz; ++i) {
-            pw1[i] = pw1[i - 1] * x % p1;
-            pw2[i] = pw2[i - 1] * x % p2;
+            pw1[i] = x * pw1[i - 1] % p1;
+            pw2[i] = x * pw2[i - 1] % p2;
         }
     }
 
@@ -34,23 +37,27 @@ public:
         q.clear();
     }
 
-    void push_back(int ch) {
-        h1 = (h1 * x % p1 + ch) % p1;
-        h2 = (h2 * x % p2 + ch) % p2;
+    void push_back(char ch) {
+        h1 = (h1 * x) % p1;
+        h1 = (h1 + ch) % p1;
+        h2 = (h2 * x) % p2;
+        h2 = (h2 + ch) % p2;
         len++;
         q.emplace_back(ch);
     }
 
-    void from_string(string s) {
-        for (int i = 0; i < s.length(); ++i)
-            push_back(s[i]);
+    void from_string(string &s) {
+        for (char i: s)
+            push_back(i);
     }
 
     void pop_front() {
-        h1 = ((h1 - q.front() * pw1[len - 1] % p1) + p1) % p1;
-        h2 = ((h2 - q.front() * pw2[len - 1] % p2) + p2) % p2;
+        if (!len)return;
+        char ch = q.front();
         q.pop_front();
         len--;
+        h1 = ((h1 - ch * pw1[len] % p1) + p1) % p1;
+        h2 = ((h2 - ch * pw2[len] % p2) + p2) % p2;
     }
 
     string get() { return string(q.begin(), q.end()); }
@@ -60,12 +67,13 @@ public:
     }
 
     void pop_back() {
-        int t = q.back();
+        if (!len)return;
+        char ch = q.back();
         q.pop_back();
-        h1 = ((h1 - t) + p1) % p1;
-        h2 = ((h2 - t) + p2) % p2;
-        h1 = h1 * power(x, p1 - 2, p1) % p1;
-        h2 = h2 * power(x, p2 - 2, p2) % p2;
+        h1 = ((h1 - ch) + p1) % p1;
+        h2 = ((h2 - ch) + p2) % p2;
+        h1 = h1 * inv1 % p1;
+        h2 = h2 * inv2 % p2;
         len--;
     }
 
@@ -76,13 +84,7 @@ public:
         q.emplace_front(ch);
     }
 
-    bool operator==(hsh &X) const {
+    bool operator==(const hsh &X) const {
         return (X.h1 == h1 and X.h2 == h2);
     }
-
-    bool operator!=(hsh &X) const {
-        return (X.h1 != h1 or X.h2 != h2);
-    }
-
 };
-
