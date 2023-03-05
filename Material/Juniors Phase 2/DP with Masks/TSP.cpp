dcd28b388b83435a31ef2da3dd5e0cebf049ca41
Abdeelaleem Ahmed <yousfahmed2@gmail.com>
2023-03-05 18:09:40 +0200

    Create TSP.cpp

---
+++ Material/Juniors Phase 2/DP with Masks/TSP.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+vector<array<int, 2>> a;
+int n, k;
+int full;
+
+double mem[19][(1 << 18) + 5];
+
+double dis(int i, int j) {
+  return ::hypot(a[i][0] - a[j][0], a[i][1] - a[j][1]);
+}
+
+double dp(int i, int msk) {
+  if (msk == full)return dis(i, k);
+  double ret = mem[i][msk];
+  if (ret == ret)return ret;
+  ret = 0;
+  for (int j = 0; j < n; ++j) {
+    if (msk & (1 << j))continue;
+    ret = min(
+        ret,
+        dp(j, msk | (1 << j)) + dis(i, j)
+    );
+  }
+  return ret;
+}
+
+
+int32_t main() {
+  std::ios_base::sync_with_stdio(false);
+  cin.tie(nullptr), cout.tie(nullptr);
+  memset(mem, -1, sizeof mem);
+  cin >> n >> k, --k;
+  full = (1 << n) - 1;
+  a.resize(n);
+  for (auto &[x, y]: a)cin >> x >> y;
+  cout << dp(k, 1 << k);
+}
