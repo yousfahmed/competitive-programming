19fea38f19266183ce74e05bb03841623211076a
Abdeelaleem Ahmed <yousfahmed2@gmail.com>
2024-06-10 11:16:29 +0300

    Update Hashing.cpp

--- templates/Strings/Hashing.cpp
+++ templates/Strings/Hashing.cpp
@@ -1,14 +1,18 @@
+const int N = 3e5 + 5;
 #define fr first
 #define sc second
-const int B1 = 256, B2 = 128;
-const int MOD1 = 2e9 + 11, MOD2 = 1e9 + 7;
+int B1, B2, MOD1, MOD2;
+int pw1[N], pw2[N];
+
 struct Hash {
+private:
   vector<pair<int, int>> prefix, suffix;
-  vector<int> p1, p2;
   int n;
 
   void hash_prefix(const string &str) {
     int h1 = 0, h2 = 0;
+    prefix.clear();
+    prefix.reserve(str.size());
     for (char i: str) {
       h1 = (1LL * h1 * B1) % MOD1;
       h1 = (h1 + i) % MOD1;
@@ -31,28 +35,26 @@ struct Hash {
   }
 
 public:
-  Hash(const string &s) {
+  Hash() {}
+
+  Hash(const string &s) { build(s); }
+
+  void build(const string &s) {
+    assert(B1);
     n = s.size();
-    p1.assign(n + 1, 1);
-    p2.assign(n + 1, 1);
-    for (int i = 1; i <= n; ++i) {
-      p1[i] = 1LL * p1[i - 1] * B1 % MOD1;
-      p2[i] = 1LL * p2[i - 1] * B2 % MOD2;
-    }
     hash_prefix(s);
-    hash_suffix(s);
+    hash_suffix(s); /// we need this ?
   }
 
-  /// revered hash to check palindrome
   pair<int, int> getSuffix(int l, int r) {
     auto ret = suffix[l];
     int len = r - l + 1;
     r++;
     if (r < n) {
-      ret.fr -= 1LL * suffix[r].fr * p1[len] % MOD1;
+      ret.fr -= 1LL * suffix[r].fr * pw1[len] % MOD1;
       if (ret.fr < 0)ret.fr += MOD1;
 
-      ret.sc -= 1LL * suffix[r].sc * p2[len] % MOD2;
+      ret.sc -= 1LL * suffix[r].sc * pw2[len] % MOD2;
       if (ret.sc < 0)ret.sc += MOD2;
 
     }
@@ -64,13 +66,33 @@ public:
     int sz = r - l + 1;
     l--;
     if (l >= 0) {
-
-      ret.fr -= 1LL * prefix[l].fr * p1[sz] % MOD1;
+      ret.fr -= 1LL * prefix[l].fr * pw1[sz] % MOD1;
       if (ret.fr < 0)ret.fr += MOD1;
-
-      ret.sc -= 1LL * prefix[l].sc * p2[sz] % MOD2;
+      ret.sc -= 1LL * prefix[l].sc * pw2[sz] % MOD2;
       if (ret.sc < 0)ret.sc += MOD2;
     }
     return ret;
   }
 };
+
+void build() {
+  if (B1)return;
+  random_device rd;
+  mt19937 mt(rd());
+  auto rnd = [&](int l, int r) { return uniform_int_distribution<int>(l, r)(mt); };
+  auto check = [](int x) {
+    for (int i = 2; i <= x / i; ++i) if (!(x % i)) return false;
+    return true;
+  };
+  B1 = rnd(100, 500);
+  B2 = rnd(100, 500);
+  MOD1 = rnd(2e8, 2e9);
+  MOD2 = rnd(2e8, 2e9);
+  while (!check(MOD1))MOD1--;
+  while (MOD1 == MOD2 or !check(MOD2))MOD2--;
+  pw1[0] = pw2[0] = 1;
+  for (int i = 1; i < N; ++i) {
+    pw1[i] = 1LL * pw1[i - 1] * B1 % MOD1;
+    pw2[i] = 1LL * pw2[i - 1] * B2 % MOD2;
+  }
+}
