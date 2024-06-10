41d11bde067211b3d7e728150b653f982a83ef4d
Abdeelaleem Ahmed <yousfahmed2@gmail.com>
2024-06-10 11:17:41 +0300

    Update SegTreeHashing.cpp

--- templates/Strings/SegTreeHashing.cpp
+++ templates/Strings/SegTreeHashing.cpp
@@ -6,25 +6,17 @@ struct Node {
   pii rev{}, fr{};
   int sz{};
 };
+const int N = 3e5 + 5;
+int MOD1, MOD2, B1, B2;
+pair<int, int> pw[N];
 
 struct SegTree {
 private:
 #define md ((lx+rx)>>1)
 #define LF (x*2+1)
 #define RT (x*2+2)
-  static const int MOD1 = 1e9 + 1, MOD2 = 2e9 + 11, B1 = 128, B2 = 51;
   vector<Node> seg;
-  vector<pii > pw;
   int n;
-
-  void pre() {
-    pw.assign(n + 1, {1, 1});
-    for (int i = 1; i <= n; ++i) {
-      pw[i].F = 1ll * B1 * pw[i - 1].F % MOD1;
-      pw[i].S = 1ll * B2 * pw[i - 1].S % MOD2;
-    }
-  }
-
 
   Node mrg(const Node &lf, const Node &rt) {
     Node res;
@@ -55,10 +47,7 @@ private:
 public:
   SegTree(const string &s) : SegTree(s.size()) { build(s, 0, 0, n - 1); }
 
-  SegTree(int n) : n(n) {
-    pre();
-    seg.assign(n << 2, {});
-  }
+  SegTree(int n) : n(n), seg(n << 2) {}
 
   Node query(int l, int r, int x = 0, int lx = 0, int rx = -1) {
     if (rx == -1)rx = n - 1;
@@ -85,3 +74,25 @@ public:
 
 #undef md
 };
+
+void build() {
+  if (B1)return;
+  random_device rd;
+  mt19937 mt(rd());
+  auto rnd = [&](int l, int r) { return uniform_int_distribution<int>(l, r)(mt); };
+  auto check = [](int x) {
+    for (int i = 2; i <= x / i; ++i) if (!(x % i)) return false;
+    return true;
+  };
+  B1 = rnd(100, 500);
+  B2 = rnd(100, 500);
+  MOD1 = rnd(2e8, 2e9);
+  MOD2 = rnd(2e8, 2e9);
+  while (!check(MOD1))MOD1--;
+  while (MOD1 == MOD2 or !check(MOD2))MOD2--;
+  pw[0] = {1, 1};
+  for (int i = 1; i < N; ++i) {
+    pw[i].F = 1LL * pw[i - 1].F * B1 % MOD1;
+    pw[i].S = 1LL * pw[i - 1].S * B2 % MOD2;
+  }
+}
