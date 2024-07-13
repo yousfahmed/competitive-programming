7c3357bf5b986ae45ca86b9ff61df2044f92b8ed
Abdeelaleem Ahmed <yousfahmed2@gmail.com>
2024-07-14 02:26:22 +0300

    Update BinaryTrie.cpp

--- templates/DS/BinaryTrie.cpp
+++ templates/DS/BinaryTrie.cpp
@@ -1,7 +1,9 @@
-/// need testing
-
 struct node {
-  int ch[2] = {-1, -1}, frq[2]{}, sz = 0;
+  int ch[2]{-1, -1}, frq[2]{}, sz = 0;
+
+  int &operator[](int x) {
+    return ch[x];
+  }
 };
 
 struct BT {
@@ -9,27 +11,28 @@ struct BT {
   vector<node> nodes;
 
   int newNode() {
-    nodes.emplace_back();
-    return nodes.size() - 1;
+    return nodes.emplace_back(), nodes.size() - 1;
   }
 
   void init() { nodes.clear(), newNode(); }
 
   BT() { init(); }
 
-  void update(int val, int op) { /// 1 -> add , -1 -> delete
+  /// +1 -> add , -1 -> delete
+  void update(int val, int op) {
     int u = 0;
     for (int i = M - 1; i >= 0; --i) {
       int v = val >> i & 1;
-      if (!~nodes[u].ch[v]) {
-        nodes[u].ch[v] = newNode();
+      if (!~nodes[u][v]) {
+        nodes[u][v] = newNode();
       }
       nodes[u].frq[v] += op;
       nodes[u].sz += op;
-      u = nodes[u].ch[v];
+      u = nodes[u][v];
     }
   }
 
+  /// helper function
   int match(int x, int y, int dep, int K) {
     if (x == -1 or y == -1)return 0;
     if (!nodes[x].sz or !nodes[y].sz)return 0;
@@ -38,11 +41,11 @@ struct BT {
       if (dep == 0) {
         return 0;
       }
-      return match(nodes[x].ch[0], nodes[y].ch[1], dep - 1, K) +
-             match(nodes[x].ch[1], nodes[y].ch[0], dep - 1, K);
+      return match(nodes[x][0], nodes[y][1], dep - 1, K) +
+             match(nodes[x][1], nodes[y][0], dep - 1, K);
     } else {
-      int n0 = match(nodes[x].ch[0], nodes[y].ch[0], dep - 1, K);
-      int n1 = match(nodes[x].ch[1], nodes[y].ch[1], dep - 1, K);
+      int n0 = match(nodes[x][0], nodes[y][0], dep - 1, K);
+      int n1 = match(nodes[x][1], nodes[y][1], dep - 1, K);
       int l0 = nodes[x].frq[0] - n0, r0 = nodes[y].frq[0] - n0;
       int l1 = nodes[x].frq[1] - n1, r1 = nodes[y].frq[1] - n1;
 
@@ -68,30 +71,29 @@ struct BT {
   int calc(int K, int x = 0, int dep = M - 1) {
     if (x == -1 or !nodes[x].sz) return 0;
     if (K >> dep & 1) {
-      return nodes[x].frq[0] + nodes[x].frq[1] - match(nodes[x].ch[0], nodes[x].ch[1], dep - 1, K);
+      return nodes[x].frq[0] + nodes[x].frq[1] - match(nodes[x][0], nodes[x][1], dep - 1, K);
     }
     return max(
-        calc(K, nodes[x].ch[0], dep - 1),
-        calc(K, nodes[x].ch[1], dep - 1)
+        calc(K, nodes[x][0], dep - 1),
+        calc(K, nodes[x][1], dep - 1)
     );
   }
 
-
   /// Count number of integers from the set that make (a[i] xor num) >=l
   int query(int num, int l) {
-    int cur = 0, ans = 0;
-    for (int i = M - 1; i >= 0 && cur != -1; i--) {
+    int u = 0, ans = 0;
+    for (int i = M - 1; i >= 0; i--) {
       int btP = num >> i & 1;
       int btL = l >> i & 1;
       if (btL) {
-        cur = nodes[cur].ch[!btP];
+        u = nodes[u][!btP];
       } else {
-        ans += nodes[cur].frq[!btP];
-        cur = nodes[cur].ch[btP];
+        ans += nodes[u].frq[!btP];
+        u = nodes[u][btP];
       }
+      if (u == -1)return ans;
     }
-    if (cur != -1)ans += nodes[cur].sz;
-    return ans;
+    return ans + nodes[u].sz;
   }
 
   /// Max value a[i] xor X
@@ -100,10 +102,10 @@ struct BT {
     for (int i = M - 1; i >= 0 && cur >= 0; --i) {
       int bt = x >> i & 1;
       if (nodes[cur].frq[!bt]) {
-        cur = nodes[cur].ch[!bt];
+        cur = nodes[cur][!bt];
         ans |= (1ll << i);
       } else {
-        cur = nodes[cur].ch[bt];
+        cur = nodes[cur][bt];
       }
     }
     return ans;
