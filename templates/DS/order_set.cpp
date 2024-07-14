f9dd4021f547668493ea597673d592f9826051f7
Abdeelaleem Ahmed <yousfahmed2@gmail.com>
2024-07-14 18:35:14 +0300

    Update order_set.cpp

--- templates/DS/order_set.cpp
+++ templates/DS/order_set.cpp
@@ -2,4 +2,4 @@
 #include <ext/pb_ds/tree_policy.hpp>
 
 using namespace __gnu_pbds;
-#define ordered_set tree<int, null_type,less<int>,  rb_tree_tag,tree_order_statistics_node_update>
+#define ordered_set tree<int, null_type,less<>,  rb_tree_tag,tree_order_statistics_node_update>
