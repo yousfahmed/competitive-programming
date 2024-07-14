f90de521b80461b832612a9f3e654a51008e36cc
Abdeelaleem Ahmed <yousfahmed2@gmail.com>
2024-07-15 01:22:04 +0300

    Update Floyd-Warshall.cpp

--- templates/Graph/Floyd-Warshall.cpp
+++ templates/Graph/Floyd-Warshall.cpp
@@ -1,17 +1,9 @@
-const int N = 200;
-int adj[N][N];
-int INF;
-
-vector<vector<int>> floyed() {
-//  memset(adj, '?', sizeof adj);
-//  INF = adj[0][0];
-  auto d = adj;
-  for (int k = 0; k < N; ++k) {
-    for (int i = 0; i < N; ++i) {
-      for (int j = 0; j < N; ++j) {
-        if (d[i][k] < INF && d[k][j] < INF)
-          d[i][j] = min(d[i][j], d[i][k] + d[k][j]);
-      }
-    }
-  }
+/// Don't forget to set the adj initially to INF (INF + INF shouldn't overflow)
+void floyd() {
+  int i, j, k;
+  for (i = 0; i < N; ++i)
+    adj[i][i] = 0;
+  for (k = 0; k < N; ++k) for (i = 0; i < N; ++i)
+      for (j = 0; j < N; ++j)
+        adj[i][j] = min(adj[i][j], adj[i][k] + adj[k][j]);
 }
