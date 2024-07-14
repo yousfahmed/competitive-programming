5fc3208dedc072e348c197bd2fe8a28106216d00
Abdeelaleem Ahmed <yousfahmed2@gmail.com>
2024-07-14 18:32:06 +0300

    Update Persistent Segment Tree.cpp

--- templates/DS/Persistent Segment Tree.cpp
+++ templates/DS/Persistent Segment Tree.cpp
@@ -1,51 +1,42 @@
-// need testing
-
-struct Node {
-  int val;
-  Node *l, *r;
-
-  Node(int x) : val(x), l(nullptr), r(nullptr) {}
-
-  Node(Node *ll, Node *rr) {
-    l = ll, r = rr;
-    val = 0;
-    if (l) val += l->val;
-    if (r) val += r->val;
-  }
-
-  Node(Node *cp) : val(cp->val), l(cp->l), r(cp->r) {}
-};
-
-int n;
-vector<Node *> roots;
-vector<int> a;
+namespace PSegTree {
 #define md ((lx+rx)>>1)
 
-Node *build(int lx = 0, int rx = n - 1) {
-  if (lx == rx) {
-    return new Node(a[lx]);
-  }
-  return new Node(build(lx, md), build(md + 1 + 1, rx));
-}
+  struct Node {
+    Node *l = 0, *r = 0;
+    int val = 0;
 
-Node *update(Node *node, int val, int pos, int lx = 0, int rx = n - 1) {
-  if (lx == rx) {
-    return new Node(val);
-  }
-  if (pos > md) {
-    return new Node(node->l, update(node->r, val, pos, md + 1, rx));
-  }
-  return new Node(update(node->l, val, pos, lx, md), node->r);
-}
+    Node(int x) : val(x), l(nullptr), r(nullptr) {}
 
-int query(Node *node, int l, int r, int lx = 0, int rx = n - 1) {
-  if (r < lx || rx < l) {
-    return 0;
+    Node(Node *l, Node *r) {
+      this->l = l, this->r = r;
+      val = 0;
+      if (l) val += l->val;
+      if (r) val += r->val;
+    }
+
+    Node(Node *cp) : val(cp->val), l(cp->l), r(cp->r) {}
+  };
+
+  int n;
+
+  Node *build(const vector<int> &a, int lx = 0, int rx = n - 1) {
+    if (lx == rx) return new Node(a[lx]);
+    return new Node(build(a, lx, md), build(a, md + 1 + 1, rx));
   }
-  if (l <= lx && rx <= r) {
-    return node->val;
+
+  Node *update(Node *node, int val, int pos, int lx = 0, int rx = n - 1) {
+    if (lx == rx) return new Node(val);
+    if (pos > md) {
+      return new Node(node->l, update(node->r, val, pos, md + 1, rx));
+    }
+    return new Node(update(node->l, val, pos, lx, md), node->r);
   }
-  return query(node->l, l, r, lx, md) + query(node->r, l, r, md + 1, rx);
-}
+
+  int query(Node *node, int l, int r, int lx = 0, int rx = n - 1) {
+    if (r < lx || rx < l) return 0;
+    if (l <= lx && rx <= r) return node->val;
+    return query(node->l, l, r, lx, md) + query(node->r, l, r, md + 1, rx);
+  }
 
 #undef md
+}
