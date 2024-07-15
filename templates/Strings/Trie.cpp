db1f0c3f4a132113b96dbf6f542938cb8f3c9cd9
Abdeelaleem Ahmed <yousfahmed2@gmail.com>
2024-07-15 23:52:29 +0300

    Update Trie.cpp

--- templates/Strings/Trie.cpp
+++ templates/Strings/Trie.cpp
@@ -1,28 +1,31 @@
 struct Node {
-  unordered_map<char, int> nxt;
-  int isEnd = 0, sz = 0;//subTree size
+	unordered_map<char, int> nxt;
+	int isEnd = 0, sz = 0;//subTree size
+	int &operator[](char x) {
+		return nxt[x];
+	}
 };
 
 struct Trie {
-  vector<Node> tr;
+	vector<Node> tr;
 
-  int newNode() {
-    tr.emplace_back();
-    return tr.size() - 1;
-  }
+	int newNode() {
+		tr.emplace_back();
+		return tr.size() - 1;
+	}
 
-  Trie() { tr.clear(), newNode(); }
+	Trie() { tr.clear(), newNode(); }
 
-  void insert(const string &s) {
-    int u = 0;
-    for (char c: s) {
-      if (!tr[u].nxt[c])
-        tr[u].nxt[c] = newNode();
-      tr[u].sz++;
-      u = tr[u].nxt[c];
-    }
-    tr[u].sz++;
-    tr[u].isEnd += 1;
-  }
+	void insert(const string &s) {
+		int u = 0;
+		for (char c: s) {
+			if (!tr[u][c])
+				tr[u][c] = newNode();
+			tr[u].sz++;
+			u = tr[u][c];
+		}
+		tr[u].sz++;
+		tr[u].isEnd++;
+	}
 
 };
