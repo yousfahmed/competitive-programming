f0e96222b127bf6e2c3b3dc31c2f8809f1d3374d
Abdeelaleem Ahmed <yousfahmed2@gmail.com>
2024-07-17 00:10:08 +0300

    Update LCA Euler tour.cpp

--- templates/Trees/LCA Euler tour.cpp
+++ templates/Trees/LCA Euler tour.cpp
@@ -1,6 +1,5 @@
-const int N = 2e5 + 5;
 vector<vector<int>> adj;
-int in[N], lvl[N];
+vector<int> in, lvl;
 vector<vector<int>> euler;
 
 void dfs0(int u, int par) {
@@ -20,25 +19,17 @@ int mrg(int u, int v) {
 
 void precompute_lca(int root = 0) {
   euler = {{}};
-  dfs0(root, -1);
+  in.assign(adj.size(), {});
+  lvl.assign(adj.size(), {});
+  dfs0(root, 0);
   const int sz = euler[0].size();
   for (int msk = 1; (1 << msk) <= sz; ++msk) {
     euler.emplace_back(sz - (1 << msk) + 1);
     for (int i = 0; i + (1 << msk) <= sz; ++i) {
       euler[msk][i] = mrg(
-          euler[msk - 1][i],
-          euler[msk - 1][i + (1 << (msk - 1))]
+        euler[msk - 1][i],
+        euler[msk - 1][i + (1 << (msk - 1))]
       );
     }
   }
-}
-
-int query(int u, int v) {
-  u = in[u], v = in[v];
-  if (u > v)swap(u, v);
-  int msk = __lg(v - u + 1);
-  return mrg(
-      euler[msk][u],
-      euler[msk][v - (1 << msk) + 1]
-  );
 }
