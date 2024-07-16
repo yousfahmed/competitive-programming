4af9b99e62abacd5790ec8c4ef07044e1ac5cf35
Abdeelaleem Ahmed <yousfahmed2@gmail.com>
2024-07-17 01:55:02 +0300

    Update Linear Sieve and Mobius.cpp

--- templates/Number Theory/Linear Sieve and Mobius.cpp
+++ templates/Number Theory/Linear Sieve and Mobius.cpp
@@ -1,23 +1,27 @@
-vi prime;
-bool isComp[N];
-int mob[N];
- 
+const int N = 1e7 + 7;
+vector<int> prime;
+bitset<N> isComp;
+char mob[N];
+int lpf[N], gpf[N];
+
 void sieve(int n = N) {
-    fill(isComp, isComp + n, false);
-    mob[1] = 1;
-    for (int i = 2; i < n; ++i) {
-        if (!isComp[i]) {
-            prime.push_back(i);
-            mob[i] = -1;
-        }
-        for (int j = 0; j < prime.size() && i * prime[j] < n; ++j) {
-            isComp[i * prime[j]] = true;
-            if (i % prime[j] == 0) {
-                mob[i * prime[j]] = 0;
-                break;
-            } else
-                mob[i * prime[j]] = mob[i] * mob[prime[j]];
-        }
+  isComp = 0;
+  mob[1] = 1;
+  for (int i = 2; i < n; ++i) {
+    if (!isComp[i]) {
+      prime.push_back(i);
+      mob[i] = -1;
+      lpf[i] = gpf[i] = i;
     }
+    for (int j = 0; j < prime.size() && i * prime[j] < n; ++j) {
+      isComp[i * prime[j]] = true;
+      lpf[prime[j] * i] = prime[j];
+      gpf[prime[j] * i] = gpf[i];
+      if (i % prime[j] == 0) {
+        mob[i * prime[j]] = 0;
+        break;
+      } else
+        mob[i * prime[j]] = mob[i] * mob[prime[j]];
+    }
+  }
 }
- 
