b210355c801bff4c70c5489ede37d11a52110ca0
Abdeelaleem Ahmed <yousfahmed2@gmail.com>
2024-07-18 01:05:40 +0300

    Create kthRoot.cpp

---
+++ templates/Utils/kthRoot.cpp
@@ -0,0 +1,16 @@
+ll kthRoot(ll n, ll k) // return integer kth root for n
+{ // Also can be done by binary search for accurate results
+  double root = pow((double) n, 1.0 / (double) k);
+  // will have percision errors
+  ll realRoot = (ll) (root - 1);
+  while (1) {
+    ll a = realRoot + 1, p = 1;
+    // compute a^k
+    for (int j = 0; j < k; j++) {
+      if (p > n / a) // we exceed n, this also detect overflow
+        return realRoot;
+      p *= a;
+    }
+    ++realRoot;
+  }
+}
