8e31b3ce90d772163a0c7ac24b7d3c038b197f9e
Abdeelaleem Ahmed <yousfahmed2@gmail.com>
2024-07-17 18:37:18 +0300

    Update Phi.cpp

--- templates/Number Theory/Phi.cpp
+++ templates/Number Theory/Phi.cpp
@@ -1,3 +1,16 @@
+int Phi(int n) {
+    int ret = n;
+    for (int i = 2; i * i <= n; i++) {
+        if (n % i == 0) {
+            while (n % i == 0)
+                n /= i;
+            ret -= ret / i;
+        }
+    }
+    if (n > 1)
+        ret -= ret / n;
+    return ret;
+}
 void Phi_1_n() {
   iota(phi, phi + N, 0);
   for (int i = 2; i < N; ++i) {
